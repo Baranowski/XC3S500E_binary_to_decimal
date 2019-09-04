a858f69efc1b10caeb3349136460ccb0820d0f42
Wojciech Baranowski <wbaranowski@protonmail.com>
2019-09-04 14:15:41 +0300

    Remove joystick support, add 4x4 keypad, display its state on LCD

--- lcd12864.v
+++ lcd12864.v
@@ -1,20 +1,39 @@
 module lcd12864(LCD_N,LCD_P,LCD_RST,PSB,clk, rs, rw, en,dat,
-				key_up, key_down, key_left, key_right, key_press);
+                key_col, key_row);
 output reg  LCD_N;
 output reg LCD_P;
 output reg LCD_RST;
 output reg PSB;
 input clk;  
-input key_up, key_down, key_left, key_right, key_press;
+
+/////// 4x4 KEYPAD
+output reg [3:0] key_col = 4'b1111;
+input [3:0] key_row;
+reg [15:0] keypad = 16'h0000;
+reg [2:0] key_i = 3'b000;
+integer keypad_iter;
+always @(posedge clk)
+begin
+  key_col[3] <= key_i[2] && key_i[1];
+  key_col[2] <= key_i[2] && !key_i[1];
+  key_col[1] <= !key_i[2] && key_i[1];
+  key_col[0] <= !key_i[2] && !key_i[1];
+  for (keypad_iter=0; keypad_iter<4; keypad_iter=keypad_iter+1)
+  begin
+    if (key_col[keypad_iter] && key_i[0])
+      keypad[keypad_iter*4+:4] <= key_row;
+    else
+      keypad[keypad_iter*4+:4] <= keypad[keypad_iter*4+:4];
+  end
+  key_i <= key_i + 1;
+end
+/////// 4x4 KEYPAD end
 
  output [7:0] dat; 
  output reg rs,rw,en; 
  //tri en; 
  reg e; 
  reg [7:0] dat; 
- reg [7:0] letter; 
- initial
-         letter<="M";
   
  reg  [31:0] counter=0; 
  reg [6:0] current=0,next=0; 
@@ -77,20 +96,6 @@ input key_up, key_down, key_left, key_right, key_press;
   
  parameter  nul=6'h35; 
 
-always @(posedge clk)
-        if (!key_up)
-                letter <= "^";
-        else if (!key_down)
-                letter <= "v";
-        else if (!key_left)
-                letter <= "<";
-        else if (!key_right)
-                letter <= ">";
-        else if (!key_press)
-                letter <= "X";
-        else
-                letter <= letter;
-
 always @(posedge clk)         //da de shi zhong pinlv 
  begin 
   counter<=counter+1; 
@@ -107,26 +112,39 @@ LCD_P<=1'b1;
 LCD_RST<=1'b1;
 PSB<=1'b1;
 end 
+
+initial
+begin
+  rs<=0;
+  dat<=8'b00000001;
+end
+
 always @(posedge clk) 
 begin 
-
  if(counter==32'hF)begin //counter==32'haff0
 		 current<=next; 
 		 case(current) 
-			 set0:   begin  rs<=0; dat<=8'h30; next<=set1; end 
-			 set1:   begin  rs<=0; dat<=8'h0c; next<=set2; end 
-			 set2:   begin  rs<=0; dat<=8'h6; next<=set3; end// 
-			 set3:   begin  rs<=0; dat<=8'h1; next<=dat0; end 
+			 set0:   begin  rs<=0; dat<=8'b00110000; next<=set1; end 
+			 set1:   begin  rs<=0; dat<=8'b00001100; next<=set2; end 
+			 set2:   begin  rs<=0; dat<=8'b00000010; next<=set4; end 
+			 set4:   begin  rs<=0; dat<=8'b00000110; next<=dat0; end// 
 		
-			 dat0:   begin  rs<=1; dat<=letter; next<=dat1; end //?????
-			 dat1:   begin  rs<=1; dat<="a"; next<=dat2; end 
-			 dat2:   begin  rs<=1; dat<="v"; next<=dat3; end 
-			 dat3:   begin  rs<=1; dat<="e";next<=dat4; end 
-			 dat4:   begin  rs<=1; dat<="s"; next<=dat5; end 
-			 dat5:   begin  rs<=1; dat<="h"; next<=dat6; end 
-			 dat6:   begin  rs<=1; dat<="a"; next<=dat7; end 
-			 dat7:   begin  rs<=1; dat<="r";next<=dat8; end 
-			 dat8:   begin  rs<=1; dat<="e"; next<=nul; end 
+			 dat0:   begin  rs<=1; dat<=keypad[15]+"0"; next<=dat1; end //?????
+			 dat1:   begin  rs<=1; dat<=keypad[14]+"0"; next<=dat2; end //?????
+			 dat2:   begin  rs<=1; dat<=keypad[13]+"0"; next<=dat3; end //?????
+			 dat3:   begin  rs<=1; dat<=keypad[12]+"0"; next<=dat4; end //?????
+			 dat4:   begin  rs<=1; dat<=keypad[11]+"0"; next<=dat5; end //?????
+			 dat5:   begin  rs<=1; dat<=keypad[10]+"0"; next<=dat6; end //?????
+			 dat6:   begin  rs<=1; dat<=keypad[9]+"0"; next<=dat7; end //?????
+			 dat7:   begin  rs<=1; dat<=keypad[8]+"0"; next<=dat8; end //?????
+			 dat8:   begin  rs<=1; dat<=keypad[7]+"0"; next<=dat9; end //?????
+			 dat9:   begin  rs<=1; dat<=keypad[6]+"0"; next<=dat10;end //?????
+			 dat10:  begin  rs<=1; dat<=keypad[5]+"0"; next<=dat11;end //?????
+			 dat11:  begin  rs<=1; dat<=keypad[4]+"0"; next<=dat12;end //?????
+			 dat12:  begin  rs<=1; dat<=keypad[3]+"0"; next<=dat13;end //?????
+			 dat13:  begin  rs<=1; dat<=keypad[2]+"0"; next<=dat14;end //?????
+			 dat14:  begin  rs<=1; dat<=keypad[1]+"0"; next<=dat15;end //?????
+			 dat15:  begin  rs<=1; dat<=keypad[0]+"0"; next<=nul; end 
 			
 			  nul:   begin rs<=0;  dat<=8'h00;                    // ????E ? ?? 
 				         if(cnt!=2'h2)  
